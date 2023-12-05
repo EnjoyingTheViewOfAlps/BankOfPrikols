6685f49d2e42e6328582a28207b625a2baa3b095
Mafiay8880@gmail.com <Mafiay8880@gmail.com>
2023-12-05 23:06:43 +0300

    First

---
+++ MyOwnSberBank/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnSberBank
+{
+    using System;
+
+    // Создаем класс для представления банковского аккаунта
+    public class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public double Balance { get; private set; }
+
+        public delegate void WithdrawalDelegate(double amount);
+
+        public WithdrawalDelegate OnWithdrawal;
+
+        public BankAccount(string accountHolder, double initialBalance)
+        {
+            AccountHolder = accountHolder;
+            Balance = initialBalance;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+
+                OnWithdrawal?.Invoke(amount);
+
+                Console.WriteLine($"Средства успешно списаны. Новый баланс: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств на счете.");
+            }
+        }
+    }
+}
---
+++ MyOwnSberBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MyOwnSberBank;
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount myAccount = new BankAccount("Иван Иванов", 1000);
+
+        myAccount.OnWithdrawal += HandleWithdrawal;
+
+        myAccount.Withdraw(500);
+
+        Console.ReadLine();
+    }
+
+    // Метод-обработчик события списания средств
+    static void HandleWithdrawal(double amount)
+    {
+        Console.WriteLine($"Списано {amount} средств со счета.");
+    }
+}
